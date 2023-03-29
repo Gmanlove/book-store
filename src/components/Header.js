8f2cf46aa321709adeb809caeac9fbb101047ee6
Gmanlove <emmagman3@gmail.com>
2023-03-29 22:06:25 +0100

    initialize project with components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './Navbar';
+
+const Header = () => (
+  <>
+    <h1>Store of Books</h1>
+    <NavBar />
+  </>
+);
+
+export default Header;
---
+++ src/routes/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => (
+  <div id="categories">
+    <button type="button">Check Out</button>
+  </div>
+);
+
+export default Categories;
